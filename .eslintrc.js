7da0d3d63d9b5d0bc4df369fd0f87da703dbbb07
damola <damolaadegbite0@gmail.com>
2025-10-18 15:18:51 +0100

    feat: Add ESLint configuration for TypeScript and Prettier integration

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+};
